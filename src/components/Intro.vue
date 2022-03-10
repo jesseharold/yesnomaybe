d76c2f3149d744a67fe999af6f79b7fd21559bfe
jesse harold <jesseharold@gmail.com>
2022-03-10 09:36:12 -0500

    clarify intro text

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -6,46 +6,58 @@
         This is a tool used for having conversations to build consent between adults for sex 
         and/or kink. This app contains sexually explicit language, and is meant as an educational tool.
       </p>
-      <h4>Using this app</h4>
+      <h4>Do I have to fill this whole thing out?</h4>
       <p>
-        For privacy reasons, this site does not collect, store, or retain any of your information. <strong>If you refresh the page,
-        the form will be reset to blank</strong>. You can privately save your sheet as a .txt file using 
-        the "Download to file" button at the bottom. Load your sheet later by pasting the contents of the file in to the field at the 
-        bottom of the page as plain text (don't use MS Word).
+        This list is long! Don't give yourself negotiation fatigue. You can collapse any category or delete any 
+        item you don't wish to fill out. Or just leave it in the
+        blank state that each row starts in, which simply indicates "no response". When sharing, let the other
+        person know you don't want to discuss all the blank items, and save your energy for the fun stuff! You can
+        always revisit later.
       </p>
+      <h4>Privacy</h4>
       <p>
-        You can collapse any category or delete any item you don't wish to fill out, or just leave it in the
-        blank state that each row starts in, which simply indicates no response.
+        This site does not, and will never collect, store, or retain any of your personal information. <strong>If you refresh the page,
+        the form will be reset to blank</strong> and all information will be fully discarded. All the save and share
+        options listed below take place on your computer for this reason. Protect your privacy by being careful with
+        how you store and share your lists. 
+      </p>
+      <h4>Saving your list</h4>
+      <p>
+        You can privately save your list as an obscured (not readable
+        by humans) ynm.txt file using the "Download to file" button at the bottom. Load your sheet later to view or
+        keep working on it by opening the ynm.txt file in a plain text
+        editor (not MS Word) and pasting the contents into the field at the bottom of the page.
       </p>
       <h4>Sharing your list</h4> 
       <p>
-        There are a few different ways to share your list after you fill it out:<br />
+        There are a few different ways to share your list after you fill it out:<br /><br />
         <b>Print</b>. Make sure all the categories you want to include are expanded, closed categories will 
         not appear on your list. Print this page, either on paper, or
-        print to a PDF and save it. (it looks best if you turn background colors on in your PDF print settings.) 
-        <br />
-        <b>Share and view in the app</b>. Email someone your ynm.txt file, and they can view it by uploading it into the app 
-        using the "Load saved" button at the bottom.
-        <br />
-        <b>Compare two lists in the app</b>. While you're viewing your own list, use the "Load and compare" 
-        button at the bottom to upload a list someone has shared with you. You will be automatically show 
-        where your answers overlap. You can choose how much to filter
+        print to a PDF and save it. (looks best if you turn background colors on in your PDF print settings.) 
+        <br /><br />
+        <b>Share and view in the app</b>. Email someone your ynm.txt file. They can view it by coming to the site
+        and uploading it, using the "Load saved list" button at the bottom.
+        <br /><br />
+        <b>Compare two lists in the app</b> (new!). While you're viewing your own list, use the "Load and compare" 
+        button at the bottom to upload a list someone has shared with you. You will be automatically shown 
+        where your "giving" answers overlap with the other person's "receiving" answers, and vice versa. 
+        You can choose how much to filter
         your overlap, whether you want to see everything, or only items where you both have enthusiastic "YES"es.
       </p>
       <h4>Filling it out together (alternate use)</h4> 
       <p>
         Traditionally, these lists are filled out separately, to give everyone time and space to think without
-        pressure. But if you prefer to fill this out together with another person, rename the first two columns
-        to be "[name of person 1] receiving" and rename the second column to be "[name of person 2] receiving".
-        Then go through the list and agree together on a shared level of interest in each person receiving each item.
+        pressure. But if you prefer to fill this out together with another person, use the little pencil icons to 
+        rename the giving/receiving columns to "[name/initial of person 1] receiving" and "[name/initial of person 2] receiving".
+        You might want to hide the experience column, since there's only one.
+        Then go through the list and agree together on a shared level of interest in each person receiving any interesting items.
         If you have different interest levels, the lower of the two should be used. "No"s from anyone should be recorded as no.
       </p>
       <h4>Consent and negotiation</h4> 
       <p>
-        Where you have overlapping yeses or maybes with another person, and talk about 
+        Where you have overlapping "yes"es or "maybe"s with another person, talk about 
         what it might look like to do it together! <b>If someone has a "no" for any item, it does not need 
-        to be discussed.</b> If someone left an item blank, it's up to you if you want to 
-        talk about it (the list is long!). Revisit your list periodically, as people's desire and openness changes all the time.
+        to be discussed.</b> Revisit your list periodically, as people's desire and openness changes all the time.
         There are lots of great resources out there <a href="https://www.rainn.org/articles/what-is-consent" 
         target="_blank">about consent</a>.
       </p>
